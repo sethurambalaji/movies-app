b30f19a857ccda822a828c2bf22a6d8f2fd794cf
sethurambalaji <73328800+sethurambalaji@users.noreply.github.com>
2021-06-16 21:16:03 +0530

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,34 +1,49 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import './Details.css';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
-class Details extends Component{
+import Typography from '@material-ui/core/Typography';
 
-    constructor(){
+class Details extends Component {
+
+    constructor() {
         super()
         this.state = {
-           movie : {}
+            movie: {}
         }
-    } 
-
-    componentDidMount(){
-        let currentState = this.state;
-        currentState.movie = moviesData.filter((movie)=>{
-            return movie.id === this.props.movieId
-        })
-        this.setState(currentState);
-        console.log(this.state.movie)
     }
 
-    render(){
-        return(<div>
-            <Header/>
-            <div className='containerDetails'>
-                <div className='leftDetails'></div>
-                <div className='middleDetails'></div>
-                <div className='rightDetails'></div>
-             </div>   
-        </div>)
+    componentWillMount() {
+        let currentState = this.state;
+        currentState.movie = moviesData.filter((movie) => {
+            return movie.id === this.props.movieId
+        })[0];
+        this.setState(currentState);
+        console.log(this.state.movie);
+    }
+
+    render() {
+        let movies = this.state.movie;
+        return (
+            <div>
+                <Header />
+                <div className='flex-containerDetails'>
+                    <div className='leftDetails'>
+                        <img src={movies.poster_url} alt={movies.title} />
+                       
+                    </div>
+
+                    <div className='middleDetails'>
+                        <Typography component="h2" variant="h2">{movies.title}</Typography>
+                        <Typography>
+                            <span className="bold">Genre:</span>
+                            {movies.genres.join(',')}
+                        </Typography>
+                    </div>
+                    <div className='rightDetails'></div>
+                </div>
+            </div>
+        )
     }
 }
 
